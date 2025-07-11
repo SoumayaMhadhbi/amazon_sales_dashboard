a19a98ef81f583f34a9a855f2782802c57d7c552
Soumaya Mhadhbi <soumaya.mhadhbi90@gmail.com>
2025-07-11 22:58:57 +0200

    update streamlit file

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if uploaded_file is not None:
     # Ajout d'un selectbox pour choisir la catégorie filtrée
     categories = [''] + prod_by_cat['category'].tolist()
     selected_category = st.selectbox("Filtrer Top 10 produits par nombre d'avis par catégorie", categories)
-
+    
+    #Top 10 produits par nombre d'avis
     st.header("Top 10 produits par nombre d'avis")
     if selected_category == '':
         top_products_rating_count = get_top_products_by_rating_count(conn)
@@ -52,6 +53,45 @@ if uploaded_file is not None:
     )
     fig.update_layout(yaxis=dict(autorange="reversed"), width=1000, height=400, margin=dict(l=300, r=10, t=40, b=40))
 
+    st.plotly_chart(fig)
+
+    #Top 10 produits avec la plus grosse remise
+    st.subheader("Top 10 produits avec la plus grosse remise")
+    top_products_discount = get_top_products_by_discount(conn)
+    top_products_discount['product_name'] = top_products_discount['product_name'].apply(
+        lambda x: x[:40] + '...' if len(x) > 40 else x)
+
+    fig = px.bar(
+        top_products_discount,
+        x='discount_percentage',
+        y='product_name',
+        orientation='h',
+        labels={'discount_percentage': 'Remise (%)', 'product_name': 'Produit'},
+        color='discount_percentage',
+        color_continuous_scale='OrRd'
+    )
+    fig.update_layout(yaxis=dict(autorange="reversed"), width=800, height=400, margin=dict(l=300, r=10, t=40, b=40))
+
+    st.plotly_chart(fig, use_container_width=True)
+    #Répartition des avis par note
+    st.header("Répartition des avis par note")
+    review_dist = get_review_distribution(conn)
+    fig1 = px.pie(review_dist, values='review_count', names='rating', color_discrete_sequence=px.colors.sequential.RdBu)
+    st.plotly_chart(fig1)
+
+    #Top 10 utilisateurs les plus actifs par nombre d'avis postés
+    st.header("Top 10 utilisateurs les plus actifs par nombre d'avis postés")
+    top_users = get_top_users_by_review_count(conn)
+    fig = px.bar(
+        top_users,
+        x='review_count',
+        y='user_name',
+        orientation='h',
+        labels={'review_count': "Nombre d'avis", 'user_name': "Utilisateur"},
+        color='review_count',
+        color_continuous_scale='Viridis'
+    )
+    fig.update_layout(yaxis={'autorange': "reversed"})
     st.plotly_chart(fig)
 
 else:
